a405dc97b345f702df7fe0777580287a8e1c79de
Dima <31822062+ponzeldima@users.noreply.github.com>
2024-04-06 00:33:51 +0300

    Time: 61 ms (45.92%) | Memory: 55 MB (7.50%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.js
+++ 1666-make-the-string-great/make-the-string-great.js
@@ -8,21 +8,11 @@ var makeGood = function(s) {
     while (!isGood) {
         isGood = true;
         for (let i = 0; i < s.length - 1; i++) {
-            if (s[i].toLowerCase() != s[i + 1].toLowerCase()) {
-                continue;
+            if (s[i] != s[i + 1] && s[i].toLowerCase() == s[i + 1].toLowerCase()) {
+                s = Array.from(s).filter((_, index) => index != i && index != i + 1).join('');
+                isGood = false;
+                break;
             }
-
-            let isOneUpperAndOneLover = 
-                (s[i].toLowerCase() == s[i] && s[i + 1].toUpperCase() == s[i + 1]) 
-                || (s[i].toUpperCase() == s[i] && s[i + 1].toLowerCase() == s[i + 1]);
-
-            if (!isOneUpperAndOneLover) {
-                continue;
-            }
-
-            s = Array.from(s).filter((_, index) => index != i && index != i + 1).join('');
-            isGood = false;
-            break;
         }
     }
     return s;
