ebc194504769864d82dc9bb83f3f622712e5efb8
Dima <31822062+ponzeldima@users.noreply.github.com>
2024-04-06 00:49:25 +0300

    Time: 48 ms (94.48%) | Memory: 51.8 MB (22.52%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.js
+++ 1666-make-the-string-great/make-the-string-great.js
@@ -4,12 +4,14 @@
  */
 var makeGood = function(s) {
     let isGood = false;
+    let lastI = 0;
     while (!isGood) {
         isGood = true;
-        for (let i = 0; i < s.length - 1; i++) {
+        for (let i = lastI; i < s.length - 1; i++) {
             if (s[i] != s[i + 1] && s[i].toLowerCase() == s[i + 1].toLowerCase()) {
                 s = s.slice(0, i) + s.slice(i + 2);
                 isGood = false;
+                lastI = (i || 1) - 1
                 break;
             }
         }
