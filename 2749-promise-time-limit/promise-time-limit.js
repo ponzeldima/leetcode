f59a259091a4acc5a3963145a09d8eb990dff515
Dima <31822062+ponzeldima@users.noreply.github.com>
2024-04-10 12:16:04 +0300

    Time: 64 ms (32.95%) | Memory: 49.5 MB (32.47%) - LeetSync

---
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return new Promise((res, rej) => {
+            setTimeout(rej, t, 'Time Limit Exceeded');
+            fn(...args).then(res).catch(rej);
+        });
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
