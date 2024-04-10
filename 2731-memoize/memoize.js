391f241953905e4abc579f36fc6c3ec31c14bd93
Dima <31822062+ponzeldima@users.noreply.github.com>
2024-04-10 11:43:16 +0300

    Time: 345 ms (15.30%) | Memory: 91.2 MB (8.82%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let memoizedValues = new Map();
+
+    return function(...args) {
+        const argsString = args.toString();
+        if(memoizedValues.has(argsString)){
+            return memoizedValues.get(argsString);
+        }
+
+        let result = fn(...args);
+        memoizedValues.set(argsString, result);
+        return result;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
